f83671651282c2519a501924ac2ee8e10dfa42a9
Quinton Brockman <qbrockman.prsvr@gmail.com>
2022-11-30 21:01:45 -0500

    11.30.22

---
+++ index.js
@@ -0,0 +1,39 @@
+const addEl = document.getElementById("add-el")
+
+function add(){
+addEl.textContent 
+}
+
+//console.log("add-el")
+
+//render color-el in javascript
+
+const colorEl = document.getElementById("color-el")
+
+function color(){
+colorEl.textContent
+}
+
+
+let fruit = ["apple", "orange", "pear", "strawberry", "banana"]
+
+for (let i = 3; i < 4; i++){
+    console.log(fruit[i])
+}
+
+var html = document.getElementById("Buy!").innerHTML ;
+function myFunction(){
+    document.getElementById("Buy!").innerHTML = xhtml ;
+}
+
+//use innerhtml to render pitbull! paragraph inside the div dogbreed
+
+const dogBreed = document.getElementById("dogbreed")
+
+dogBreed.innerHTML = "<p> pitbull </p>"
+
+//use innerhtml to render m&m! paragraph inside the div candy
+
+let candy = document.getElementById("candy")
+
+candy.innerHTML = "<p> m&m </p>"
